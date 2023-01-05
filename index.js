f747d3fb44f83777031aa3ae8e233393ed903cb4
pradhumn1507 <117638511+pradhumn1507@users.noreply.github.com>
2023-01-05 15:36:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const app =require('./app.js');
-app.listen('3000',()=>{
-  console.log("Listening on port 3000");
-})
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/main.html'));
+});
+//your code here
+app.post('/add', (req, res) => {
+  const {a,b} = req.body;
+  res.status(200).send(a+b);
+  // res.sendFile(path.join(__dirname + '/main.html'));
+});
+module.exports = app;
